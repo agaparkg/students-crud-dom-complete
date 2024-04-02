98a870f8126fbdc657f0e6f723e8c1625d5c5d12
Azamat Gaparov <agaparovkg@gmail.com>
2024-04-01 17:40:50 -0700

    New changes

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const modalTitle = document.querySelector(".modal-header h1");
 const modalBody = document.querySelector("#myModal .modal-body");
 const modalFooter = document.querySelector("#myModal .modal-footer");
 
+let students = [];
+
 addStudentBtn.addEventListener("click", () => {});
 
 selectSortBy.addEventListener("change", (e) => {
@@ -26,9 +28,70 @@ selectSortBy.addEventListener("change", (e) => {
 });
 
 function initApp() {
+  //   bsSpinner.classList.add("d-none");
+  //   bsSpinner.classList.remove("d-none");
   bsSpinner.classList.toggle("d-none");
 
-  getStudents().then((data) => console.log(data));
+  //   const data = await getStudents();
+
+  getStudents().then((data) => {
+    bsSpinner.classList.toggle("d-none");
+    students = data;
+    createStudents(students);
+  });
+  //   setTimeout(() => {}, 2000);
+
+  //   bsSpinner.classList.add("d-none");
+
+  //   getStudents().then((data) => console.log(data));
 }
 
 initApp();
+
+function createStudents(students) {
+  //   students = [];
+  tableBody.innerHTML = "";
+
+  if (!students.length) {
+    tableBody.innerHTML = `
+        <tr>
+            <td colspan="8">No data found!</td>
+        </tr>
+    `;
+    return;
+  }
+
+  for (let student of students) {
+    createSingleStudent(student);
+  }
+}
+
+function createSingleStudent({ id, fname, lname, age, avatar, email, github }) {
+  //   const { id, fname, lname, age, avatar, email, github } = student;
+  const studentEl = `
+            <tr>
+                <td>${id}</td>
+                <td>
+                    <img src="${avatar}" alt="" />
+                </td>
+                <td>${fname}</td>
+                <td>${lname}</td>
+                <td>${age}</td>
+                <td>${email}</td>
+                <td>${github}</td>
+                <td>
+                <button id="edit" type="button" class="btn btn-secondary">
+                    <i class="bi bi-pencil"></i>
+                </button>
+                <button id="delete" type="button" class="btn btn-danger">
+                    <i class="bi bi-trash"></i>
+                </button>
+                <button id="info" type="button" class="btn btn-primary">
+                    <i class="bi bi-info-circle-fill"></i>
+                </button>
+                </td>
+            </tr>
+  `;
+
+  tableBody.innerHTML += studentEl;
+}
